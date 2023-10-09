81c1cd5b1bb8663c300cf2a2773109c2637f86ae
sultan <sultan.aiken@gmail.com>
2023-10-09 17:55:44 +0600

    213

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -36,16 +40,99 @@ func main() {
 }
 
 func login(ctx *gin.Context) {
+	var user User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	var existingUser User
+	if err := db.Where("username = ?", user.Username).First(&existingUser).Error; err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	if !checkPassword(user.Password, existingUser.Password) {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+	})
+
+	tokenString, err := token.SignedString([]byte("your-secret-key"))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating token"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"token": tokenString})
 }
 func register(ctx *gin.Context) {
+	var user User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	hashedPassword := hashPassword(user.Password)
+	user.Password = hashedPassword
+
+	if err := db.Create(&user).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
 }
 
 func authMiddleware(ctx *gin.Context) {
+	tokenString := ctx.GetHeader("Authorization")
+	if tokenString == "" {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
+		ctx.Abort()
+		return
+	}
 
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte("your-secret-key"), nil
+	})
+
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		ctx.Abort()
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		ctx.Abort()
+		return
+	}
+
+	username, ok := claims["username"].(string)
+	if !ok {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		ctx.Abort()
+		return
+	}
+
+	ctx.Set("username", username)
+	ctx.Next()
 }
 
 func protectedHandler(ctx *gin.Context) {
+	username := ctx.GetString("username")
+	ctx.JSON(http.StatusOK, gin.H{"message": "You are authorized as " + username})
+}
+func hashPassword(password string) string {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashedPassword)
+}
 
+func checkPassword(password, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
 }
